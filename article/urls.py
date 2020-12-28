4ffbe37e73bc098f0041471d5a1ada290fa6b901
xylander <xylander23@sohu.com>
2020-12-28 23:42:28 +0800

    TEST: 增加article的视图功能（测试）

--- article/urls.py
+++ article/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from . import views
+
 
 app_name = 'article'
 
 urlpatterns = [
-
+    # path函数将url映射到视图
+    path("article-list/", views.article_list, name="article_list")
 ]
