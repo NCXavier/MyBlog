4e101512d71c402cfb8c58ac1b27d0194f96c389
xylander <xylander23@sohu.com>
2020-12-29 23:10:16 +0800

    Feat: 新增视图功能 1. 修改article的view.py 2. 增加了temples文件夹，并简单加了一个list文件 3. 在settings.py里面加入初始配置

--- article/views.py
+++ article/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import ArticlePost
+
 # Create your views here.
 
 def article_list(request):
-    return HttpResponse("Hello World!")
+    # 取出所有博客文章
+    articles = ArticlePost.objects.all()
+    # 需要传递给模板的对象
+    context = {"articles": articles}
+    return render(request, 'article/list.html', context)
