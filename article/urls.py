c07db27f16f5e473aabd8c9135dbdbae014fe57c
xylander <xylander23@sohu.com>
2021-01-10 13:04:31 +0800

    Fix: 修复因为写法引起的问题

--- article/urls.py
+++ article/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # path函数将url映射到视图
     path("article-list/", views.article_list, name="article_list"),
     # 文章详情
-    path("article-detail/<int:id>/", views.article_detail, name="article_detail")
+    path("article-detail/<int:id>/", views.article_detail, name="article_detail"),
     # 写文章
     path("article-create/", views.article_create, name="article_create")
 ]
