5f624581a695ae120ab04bc41d8415f1d518f405
xylander <xylander23@sohu.com>
2021-01-10 14:30:58 +0800

    Feat: 删除文章功能

--- article/urls.py
+++ article/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     # 文章详情
     path("article-detail/<int:id>/", views.article_detail, name="article_detail"),
     # 写文章
-    path("article-create/", views.article_create, name="article_create")
+    path("article-create/", views.article_create, name="article_create"),
+    # 删除文章
+    path("article-delete/<int:id>/", views.article_delete, name="article_delete")
 ]
