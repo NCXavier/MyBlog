5f867659924eb904cb4a4161209fd6f862fcd3d9
xylander <xylander23@sohu.com>
2021-01-18 22:41:22 +0800

    Test

--- userprofile/views.py
+++ userprofile/views.py
@@ -39,24 +39,24 @@ def user_logout(request):
 
 
 def user_register(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         user_register_form = UserRegisterForm(data=request.POST)
         if user_register_form.is_valid():
             new_user = user_register_form.save(commit=False)
             # 设置密码
-            new_user.set_password(user_register_form.cleaned_data["password"])
+            new_user.set_password(user_register_form.cleaned_data['password'])
             new_user.save()
             # 保存好数据后立即登录并返回博客列表页面
             login(request, new_user)
             return redirect("article:article_list")
         else:
-            return HttpResponse("注册表单输入有误，请重新输入～")
-    elif request.method == "GET":
+            return HttpResponse("注册表单输入有误。请重新输入~")
+    elif request.method == 'GET':
         user_register_form = UserRegisterForm()
-        context = {"form": user_register_form}
-        return render(request, "userprofile/register.html", context)
+        context = { 'form': user_register_form }
+        return render(request, 'userprofile/register.html', context)
     else:
-        return HttpResponse("请使用GET或POST请求数据")
+        return HttpResponse("请使用GET或POST请求数据")s
 
 
 @login_required(login_url="/userprofile/login/")
